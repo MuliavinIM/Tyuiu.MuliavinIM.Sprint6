462c2bf42ef43b1487f8690565586ad67c7bcda6
Иван Мулявин <Tyuiu.MuliavinIM@outlook.com>
2025-12-25 19:06:46 +0500

    Task4

---
+++ Tyuiu.MuliavinIM.Sprint6.Task4.V25.Lib/DataService.cs
@@ -0,0 +1,21 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint6;
+namespace Tyuiu.MuliavinIM.Sprint6.Task4.V25.Lib
+{
+    public class DataService : ISprint6Task4V25
+    {
+        public double[] GetMassFunction(int startValue, int stopValue)
+        {
+            int len = stopValue - startValue + 1;
+            double[] resultArray = new double[len];
+            int count = 0;
+
+            for (int x = startValue; x <= stopValue; x++)
+            {
+                double result = Math.Cos(x) + (4 * x) / 2 - Math.Sin(x) * 3 * x;
+                resultArray[count] = Math.Round(result, 2);
+                count++;
+            }
+            return resultArray;
+        }
+    }
+}
